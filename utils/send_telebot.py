5633fe7ff5b6ded87dfc9b68b330904f55110732
kevinsa@gmail.com <liukaiwen@kuaishou.com>
2022-03-25 17:41:17 +0800

    fix:telebot set bug && add:basic in sonatype

--- utils/send_telebot.py
+++ utils/send_telebot.py
@@ -6,10 +6,10 @@ import telebot
 
 class SendTelebot:
     def __init__(self):
-        self.token = ""
-        self.chat_id = 0
+        self.token = "5062609907:AAHaQ6QE3omoQiz15YdBjKKj13TUWG9fzaQ"
+        self.chat_id = 773295023
 
     def send_message(self, text, parse_mode):
         logging.info("send message")
         tb = telebot.TeleBot(self.token)
-        tb.send_message(self.chat_id, text=json.dumps(text), parse_mode=parse_mode)
+        tb.send_message(self.chat_id, text=json.dumps(text))
