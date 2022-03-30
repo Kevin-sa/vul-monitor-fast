06f51f3247a07a5ce145cae1a19aea23057b5341
kevinsa@gmail.com <liukaiwen@kuaishou.com>
2022-03-30 17:39:58 +0800

    fix:telebot bug

--- utils/send_telebot.py
+++ utils/send_telebot.py
@@ -6,10 +6,15 @@ import telebot
 
 class SendTelebot:
     def __init__(self):
-        self.token = "5062609907:AAHaQ6QE3omoQiz15YdBjKKj13TUWG9fzaQ"
-        self.chat_id = 773295023
+        self.token = ""
+        self.chat_id = 0
 
     def send_message(self, text, parse_mode):
         logging.info("send message")
         tb = telebot.TeleBot(self.token)
-        tb.send_message(self.chat_id, text=json.dumps(text))
+        tb.config['api_key'] = self.token
+        return tb.send_message(self.chat_id, text=json.dumps(text))
+
+
+if __name__ == "__main__":
+    print(SendTelebot().send_message({'text': 'test'}, None))
